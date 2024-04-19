9f1bd999b3f1585f19f996be8ffd50a0de7e5a1d
Brian Lee <bnlee419@gmail.com>
2024-04-18 19:22:30 -0700

    server proxy, shadcnui, react router, login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './utils/routes';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  // return 'hello';
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div className="text-xl">REGISTER PAGE</div>;
+};
+
+export default Register;
