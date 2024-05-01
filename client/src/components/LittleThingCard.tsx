0d25f6d84b5a86c21f9d096d617d3c20d724d5ca
Brian Lee <bnlee419@gmail.com>
2024-04-30 20:39:28 -0700

    completion progress

--- client/src/components/LittleThingCard.tsx
+++ client/src/components/LittleThingCard.tsx
@@ -68,6 +68,21 @@ const LittleThingCard = ({
               <p className="text-xl font-medium text-gray-900 truncate dark:text-white">
                 <span className="text-lt-green-2">Little</span> Thing:{' '}
                 {littlething}
+                <span className="inline-flex items-end ml-2">
+                  <a
+                    href="#_"
+                    className="relative px-2 py-0 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-full shadow-inner group"
+                  >
+                    <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+                    <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+                    <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                    <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                    <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-lt-green-2 opacity-0 group-hover:opacity-100"></span>
+                    <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
+                      +
+                    </span>
+                  </a>
+                </span>
               </p>
             </div>
           </div>
