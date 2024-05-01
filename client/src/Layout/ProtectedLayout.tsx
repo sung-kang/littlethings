1c9ed6c9d9419f7d2c8e93eee7bab7c6812f70e7
Sung Ku Kang <sungkuka@gmail.com>
2024-04-30 20:13:58 -0700

    add dark mode to layouts and add styling

--- client/src/Layout/ProtectedLayout.tsx
+++ client/src/Layout/ProtectedLayout.tsx
@@ -1,10 +1,13 @@
-import { NavBar } from '@/components';
+import { NavBar, ToggleTheme } from '@/components';
 import { Outlet } from 'react-router-dom';
 
 const ProtectedLayout = () => {
   return (
     <main className="flex h-screen">
       <NavBar />
+      <div className="absolute top-4 right-4 z-10">
+        <ToggleTheme />
+      </div>
       <div className="flex-grow overflow-auto">
         <Outlet />
       </div>
